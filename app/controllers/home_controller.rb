5c7f2daa6b3d6c9fbf762c7e7d930c82fa2813aa
Adrian Chin <syncing95@gmail.com>
2022-02-19 21:33:55 +0800

    added prices and procfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,39 +2,16 @@ require "uri"
 require "net/http"
 
 class HomeController < ApplicationController
-  def index
-    # @ancUstValue = AncUstProvider.call()
-    # @lunaUstValue = LunaUstProvider.call()
-    # @osmoUstValue = OsmoUstProvider.call()
+  def index    
+    @luna_ust_value = LunaUstProvider.call()
+    @osmo_ust_value = OsmoUstProvider.call()
 
-    @lateQuartetValue = LateQuartetProvider.call()
+    @late_quartet_value = LateQuartetProvider.call()
+    
+    # @luna_ust_value = 504.23
+    # @osmo_ust_value = 1023.90
 
-    @ancUstValue = 1234.23
-    @lunaUstValue = 504.23
-    @osmoUstValue = 1023.90
-
-    query = {
-      "ids" => "anchor-protocol,osmosis,terra-luna,cosmos",
-      "vs_currencies" => "usd",
-      "include_24hr_change" => "true"
-  }
-
-  url =
-  'https://api.coingecko.com/api/v3/simple/price'
-  coinPrices = HTTParty.get(url, :query => query).parsed_response  
-  p coinPrices
-  p coinPrices.keys
-
-  coinArray = []
-  coinPrices.keys.each { |key|
-    coinPrices[key]["coingecko_id"] = key
-    coinPrices[key]["price"] = coinPrices[key]["usd"]
-    coinPrices[key]["price_change_24h"] = coinPrices[key]["usd_24h_change"]
-    coinArray << coinPrices[key].except("usd", "usd_24h_change")
-  }
-
-  # p coinArray
-  
-  Coin.upsert_all(coinArray, unique_by: :coingecko_id)
+    @osmo_price = Coin.find_by(coingecko_id: 'osmosis')["price"]
+    @luna_price = Coin.find_by(coingecko_id: 'terra-luna')["price"]
   end
 end
--- app/workers/get_coin_prices.rb
+++ app/workers/get_coin_prices.rb
@@ -5,18 +5,23 @@ class GetCoinPrices
 
   def perform
     query = {
-        "ids" => "anchor-protocol,osmosis,terra-luna,cosmos",
-        "vs_currencies" => "usd",
-        "include_24hr_change" => "true"
+      "ids" => "anchor-protocol,osmosis,terra-luna,cosmos",
+      "vs_currencies" => "usd",
+      "include_24hr_change" => "true"
     }
 
     url =
     'https://api.coingecko.com/api/v3/simple/price'
-    coinPrices = HTTParty.get(url, :query => query).parsed_response  
-    p coinPrices
-    p coinPrices.keys
+    coinPrices = HTTParty.get(url, :query => query).parsed_response      
 
-    # anchorCoin = Coin.new(coingecko_id: 'anchor-protocol', price: coinPrices["anchor-protocol"]["usd"], price_change_24h: coinPrices["anchor-protocol"]["usd_24h_change"])
-    # anchorCoin.save
+    coinArray = []
+    coinPrices.keys.each { |key|
+      coinPrices[key]["coingecko_id"] = key
+      coinPrices[key]["price"] = coinPrices[key]["usd"]
+      coinPrices[key]["price_change_24h"] = coinPrices[key]["usd_24h_change"]
+      coinArray << coinPrices[key].except("usd", "usd_24h_change")
+    }    
+    
+    Coin.upsert_all(coinArray, unique_by: :coingecko_id)
   end
 end
