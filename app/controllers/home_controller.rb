d1fc33c5f707e45cbc58fccb0ce9b14fd613f0b5
Adrian Chin <syncing95@gmail.com>
2022-02-20 21:04:04 +0800

    initial commit for card design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,13 @@ require "net/http"
 
 class HomeController < ApplicationController
   def index    
-    @luna_ust_value = LunaUstProvider.call()
-    @osmo_ust_value = OsmoUstProvider.call()
+    # @luna_ust_value = LunaUstProvider.call()
+    # @osmo_ust_value = OsmoUstProvider.call()
 
     @late_quartet_value = LateQuartetProvider.call()
     
-    # @luna_ust_value = 504.23
-    # @osmo_ust_value = 1023.90
+    @luna_ust_value = 504.23
+    @osmo_ust_value = 1023.90
 
     @osmo_price = Coin.find_by(coingecko_id: 'osmosis')["price"]
     @luna_price = Coin.find_by(coingecko_id: 'terra-luna')["price"]
