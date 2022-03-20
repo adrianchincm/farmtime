064818d097e536c265b851cfbf936bcc52dea125
Adrian Chin <syncing95@gmail.com>
2022-03-20 17:31:10 +0800

    update to use Pool values

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,15 @@ require "net/http"
 
 class HomeController < ApplicationController
   def index    
-    # @luna_ust_value = LunaUstProvider.call() # initial amount : 520 + 560 = 1070
-    # @osmo_ust_value = OsmoUstProvider.call() # initial amount : 975
-    # @atom_osmo_value = OsmoUstProvider.call() # atom/osmo , initial amount : 575
+    @luna_ust_value = Pool.find_by(name: "LUNA-UST").current_price # initial amount : 520 + 560 = 1070
+    @osmo_ust_value = Pool.find_by(name: "OSMO-UST").current_price # initial amount : 975
+    @atom_osmo_value = Pool.find_by(name: "ATOM-OSMO").current_price # atom/osmo , initial amount : 575
 
-    @late_quartet_value = LateQuartetUpdater.call() # initial amount : 236
+    @late_quartet_value = Pool.find_by(name: "Late Quartet").current_price # initial amount : 236
     
-    @luna_ust_value = 504.23
-    @osmo_ust_value = 1023.90
-    @atom_osmo_value = 1002.72
+    # @luna_ust_value = 504.23
+    # @osmo_ust_value = 1023.90
+    # @atom_osmo_value = 1002.72
 
     @osmo_price = Coin.find_by(coingecko_id: 'osmosis')["price"]
     @luna_price = Coin.find_by(coingecko_id: 'terra-luna')["price"]
--- app/services/late_quartet_updater.rb
+++ app/services/late_quartet_updater.rb
@@ -5,14 +5,16 @@ class LateQuartetUpdater < ApplicationService
   
     def call
       url =
-        'https://api.debank.com/portfolio/project_list?user_addr=0xf7865467a13b46c468b53e067533facae968fc3d'
+        'https://openapi.debank.com/v1/user/complex_protocol_list?id=0xF7865467a13B46c468B53e067533FacaE968FC3d&chain_id=ftm'
       getLateQuartet = HTTParty.get(url).parsed_response
+
+      if (getLateQuartet[0]["id"] == "ftm_beefy")
+        lateQuarterValue = getLateQuartet[0]["portfolio_item_list"][0]["stats"]["asset_usd_value"]
+        pool = Pool.find_by(tokens: ["fantom", "bitcoin", "ethereum", "usd-coin"])
+        pool.current_price = lateQuarterValue
+        pool.save
+      end          
       
-      lateQuarterValue = getLateQuartet["data"][0]["portfolio_list"][0]["stats"]["asset_usd_value"]
-      
-      pool = Pool.find_by(tokens: ["fantom", "bitcoin", "ethereum", "usd-coin"])
-      pool.current_price = lateQuarterValue
-      pool.save
     end
   end
   
