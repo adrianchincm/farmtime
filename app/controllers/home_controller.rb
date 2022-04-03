5b643f84425ec322bf03114af2490c7eb990cc3e
Adrian Chin <syncing95@gmail.com>
2022-04-03 23:08:39 +0800

    init commit display charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,24 +4,16 @@ require "net/http"
 class HomeController < ApplicationController
   def index
     @pools = Pool.all.sort_by(&:pool_owner)
-    
-    @luna_ust_pool = Pool.find_by(name: "LUNA-UST")
-    @luna_ust_value = Pool.find_by(name: "LUNA-UST").current_price # initial amount : 520 + 560 = 1070
-    @osmo_ust_value = Pool.find_by(name: "OSMO-UST").current_price # initial amount : 975
-    @atom_osmo_value = Pool.find_by(name: "ATOM-OSMO").current_price # atom/osmo , initial amount : 575
+  
+    @chart_data = PoolHourly.find_by(pool_id: 6)
 
-    @late_quartet_value = Pool.find_by(name: "Late Quartet").current_price # initial amount : 236
-    
-    # @luna_ust_value = 504.23
-    # @osmo_ust_value = 1023.90
-    # @atom_osmo_value = 1002.72
+    pool_hourly = PoolHourly.where(pool_id: 6)
 
-    @osmo_price = Coin.find_by(coingecko_id: 'osmosis')["price"]
-    @luna_price = Coin.find_by(coingecko_id: 'terra-luna')["price"]
-    @atom_price = Coin.find_by(coingecko_id: 'cosmos')["price"]
+    @pool_hash = Hash.new
+    pool_hourly.each { |hourly|  
+      @pool_hash[hourly.created_at.strftime("%d-%m-%Y")] = hourly.current_price      
+    }
 
-    @btc_price = Coin.find_by(coingecko_id: 'bitcoin')["price"]
-    @eth_price = Coin.find_by(coingecko_id: 'ethereum')["price"]
-    @ftm_price = Coin.find_by(coingecko_id: 'fantom')["price"]
+    puts "POOL HASH #{@pool_hash}"
   end
 end
