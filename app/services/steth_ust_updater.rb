1be632b725fbdd1ab776014739ac7101abb87b15
Adrian Chin <syncing95@gmail.com>
2022-04-03 16:15:23 +0800

    add padding, remove puts

--- app/services/steth_ust_updater.rb
+++ app/services/steth_ust_updater.rb
@@ -26,9 +26,7 @@ class StethUstUpdater < ApplicationService
         
       # get staked pool value
       value_per_lp_token = pool_tvl / total_lp_tokens
-      steth_ust_pool_value = user_lp_tokens * value_per_lp_token
-
-      puts "TOTAL VALUE = #{steth_ust_pool_value}"
+      steth_ust_pool_value = user_lp_tokens * value_per_lp_token      
       
       pool = Pool.find_by(tokens: ["wrapped-steth", "terrausd"])
       pool.current_price = steth_ust_pool_value
