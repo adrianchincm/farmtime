b275cc710be9ae1cf424f1a19fede3d5828483e8
Adrian Chin <syncing95@gmail.com>
2022-04-10 13:41:43 +0800

    init commit for charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,13 @@ class HomeController < ApplicationController
   def index
     @pools = Pool.all.sort_by(&:pool_owner)
       
-    # pool_hourly = PoolHourly.where(pool_id: 6)
+    pool_hourly = PoolHourly.where(pool_id: 6)
 
-    # @pool_hash = Hash.new
-    # pool_hourly.each { |hourly|  
-    #   @pool_hash[hourly.created_at] = hourly.current_price      
-    # }
+    @pool_hash = Hash.new
+    pool_hourly.each { |hourly|  
+      @pool_hash[hourly.created_at] = hourly.current_price      
+    }
 
-    # puts "POOL HASH #{@pool_hash}"
+    puts "POOL HASH #{@pool_hash}"
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,7 +9,7 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'chartkick', to: 'chartkick.js'
 pin 'Chart.bundle', to: 'Chart.bundle.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'highcharts' # @9.3.3
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "highcharts" # @10.0.0
