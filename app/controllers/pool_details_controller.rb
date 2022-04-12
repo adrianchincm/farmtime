874e5a661b53c35ae7413fa2517fd6fc91917bfe
Adrian Chin <syncing95@gmail.com>
2022-04-12 23:46:11 +0800

    init commit for portfolio pool page

--- app/controllers/pool_details_controller.rb
+++ app/controllers/pool_details_controller.rb
@@ -7,10 +7,15 @@ class PoolDetailsController < ApplicationController
         @pool = Pool.find_by(id: @pool_id, portfolio_id: portfolio.id) 
         @previous_daily = PreviousDailyProvider.call(@pool_id, portfolio.id)
         @previous_daily_pnl = get_percentage_pnl(@previous_daily).round(2)
+        @previous_daily_pnl_amount = get_pnl_amount(@previous_daily).round(2)
     end
 
     def get_percentage_pnl(previous_daily)
         ((@pool.current_price - previous_daily) / previous_daily) * 100
+    end
+
+    def get_pnl_amount(previous_daily)
+        @pool.current_price - previous_daily
     end
     
 end
