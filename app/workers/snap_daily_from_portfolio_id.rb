5c8d449b4c5eebfa896a1da598397e38f28f2472
Adrian Chin <syncing95@gmail.com>
2022-04-19 17:22:20 +0800

    rescue if not enough data from hourlies

--- app/workers/snap_daily_from_portfolio_id.rb
+++ app/workers/snap_daily_from_portfolio_id.rb
@@ -6,10 +6,14 @@ class SnapDailyFromPortfolioId
   def perform(portfolio_id)
     yesterday = Date.today - 1
     pools = Pool.where(portfolio_id: portfolio_id)
-    pools.each do |pool|    
+    pools.each do |pool|
         hourlies = PoolHourly.where(created_at: yesterday.beginning_of_day..yesterday.end_of_day, pool_id: pool.id)
-        daily_average = hourlies.sum(&:current_price) / hourlies.size
+        if hourlies.empty?
+          daily_average = Pool.find(pool_id: pool.id, portfolio_id: portfolio_id).current_price
+        else
+          daily_average = hourlies.sum(&:current_price) / hourlies.size
+        end
         PoolDaily.create(pool_id: pool.id, current_price: daily_average, portfolio_id: portfolio_id)
-    end    
+    end
   end
 end
