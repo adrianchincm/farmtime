8d05657d6c166c951b6b9a8072623e6ada613af9
Adrian Chin <syncing95@gmail.com>
2022-04-26 23:57:23 +0800

    add service object to fill missing coindix stats

---
+++ app/services/pool_stat_daily/missing_daily_stat_updater.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class MissingDailyStatUpdater < ApplicationService  
+    def initialize(coindix_id)
+        @coindix_id = coindix_id      
+    end
+  
+    def call        
+        latest_pool_stat = PoolStatDaily.where(coindix_id: @coindix_id[:coindix_id]).last
+        latest_pool_stat.created_at = DateTime.parse("2022-04-23T02:0:00.000Z")
+
+        url = "https://api.coindix.com/vaults/#{@coindix_id[:coindix_id]}?period=365"        
+        coindix_response = HTTParty.get(url, headers: { "Authorization" => "Bearer #{Figaro.env.coindix_bearer_token}", "User-Agent" => "PostmanRuntime/7.29.0" }).parsed_response
+        series = coindix_response["series"]
+
+        seed_dates = series.select {|daily| DateTime.parse(daily["date"]) > latest_pool_stat.created_at}
+        
+        seed_dates.each do |seed_date|
+            PoolStatDaily.create(name: coindix_response["name"], tvl: seed_date["tvl"], apy: seed_date["apy"].to_f * 100, coindix_id: @coindix_id[:coindix_id], created_at: seed_date["date"])
+        end        
+    end
+  end
+  
