dc6059ef8fe48280e6f7c04f525f7c04d7002e9e
Adrian Chin <syncing95@gmail.com>
2022-04-30 23:10:24 +0800

    add spec finance steth-ust updater

---
+++ app/services/steth_ust_spec_finance_updater.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class StethUstSpecFinanceUpdater < ApplicationService
+    def (portfolio)
+        @portfolio = portfolio
+    end
+  
+    def call
+      # get Pool TVL
+      url =
+        'https://fcd.terra.dev/wasm/contracts/terra1edurrzv6hhd8u48engmydwhvz8qzmhhuakhwj3/store?query_msg={%22pool%22:{}}'
+      get_pool_response = HTTParty.get(url).parsed_response
+  
+      steth_in_usd = (get_pool_response["result"]["assets"][0]["amount"].to_f / 100000000) * Coin.find_by(coingecko_id: 'wrapped-steth').price
+      ust = get_pool_response["result"]["assets"][1]["amount"].to_f / 1000000
+  
+      pool_tvl = steth_in_usd + ust
+  
+      total_lp_tokens = get_pool_response["result"]["total_share"].to_f / 1000000  
+        
+      # get Total LP Tokens
+      url =
+        'https://hive.terra.dev/graphql'
+      get_user_lp_tokens = HTTParty.post(url, body: "{\"query\":\"{stakedterra1qz4cv5lsfw4k2266q52z9rtz64n58paxy9d476: wasm {\\n            contractQuery(\\n              contractAddress: \\\"terra1zgrx9jjqrfye8swykfgmd6hpde60j0nszzupp9\\\"\\n              query: {\\n                deposit: {\\n                  lp_token: \\\"terra1qz4cv5lsfw4k2266q52z9rtz64n58paxy9d476\\\"\\n                  user: \\\"#{@portfolio.terra_address}\\\"\\n                }\\n              }\\n            )\\n          }\\n}\",\"variables\":{}}",
+      headers: { 'Content-Type' => 'application/json' }).parsed_response
+
+      url =
+       'https://lcd.terra.dev/wasm/contracts/terra12td8as6zhm3m9djjmpxzfue9syvrj0ewe070hf/store?query_msg=%7B%22reward_info%22:%7B%22staker_addr%22:%22terra1n5rkxxys26v0ks0yptp0a7v9wt5v3q85g4s4hq%22%7D%7D'
+      get_spec_staked_lp_tokens = HTTParty.get(url).parsed_response
+
+      user_lp_tokens = get_spec_staked_lp_tokens["result"]["reward_infos"][0]["auto_bond_amount"]      
+        
+      # get staked pool value
+      value_per_lp_token = pool_tvl / total_lp_tokens
+      
+      steth_ust_pool_value = user_lp_tokens * value_per_lp_token          
+      
+      pool = Pool.find_by(portfolio_id: @portfolio.id, tokens: ["wrapped-steth", "terrausd"])
+      pool.current_price = steth_ust_pool_value
+      pool.save     
+    end
+  end
+    
