240148b97591655f96d11e82c8b01d0197c5c643
Adrian Chin <syncing95@gmail.com>
2022-05-04 01:34:45 +0800

    Update spectrum finance pool stat daily updater

--- app/services/pool_stat_daily/spectrum_finance_pool_stat_daily_updater.rb
+++ app/services/pool_stat_daily/spectrum_finance_pool_stat_daily_updater.rb
@@ -6,7 +6,7 @@ class SpectrumFinancePoolStatDailyUpdater < ApplicationService
   
     def call
       pool_stat = PoolStat.find_by(tokens: ["wrapped-steth", "terrausd"] , pool_owner: "Spectrum Finance")
-      PoolStatDaily.create(name: "Spectrum Finance - BTC/ETH", tvl: pool_stat.tvl, apy: pool_stat.apr, coindix_id: nil)
+      PoolStatDaily.create(name: "Spectrum Finance - WSTETH/UST", tvl: pool_stat.tvl, apy: pool_stat.apr, farmtime_id: "spectrum-finance-wsteth-ust")
     end
   end
   
