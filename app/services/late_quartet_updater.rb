b20c74dff1adf77069e56dfc74f06c9116adeac2
Adrian Chin <syncing95@gmail.com>
2022-05-04 22:06:48 +0800

    fix updater when pool has been liquidated

--- app/services/late_quartet_updater.rb
+++ app/services/late_quartet_updater.rb
@@ -10,7 +10,7 @@ class LateQuartetUpdater < ApplicationService
         "https://openapi.debank.com/v1/user/complex_protocol_list?id=#{@portfolio.fantom_address}&chain_id=ftm"
       getLateQuartet = HTTParty.get(url).parsed_response
 
-      if (getLateQuartet[0]["id"] == "ftm_beefy")
+      if (!getLateQuartet.empty? && getLateQuartet[0]["id"] == "ftm_beefy")
         lateQuarterValue = getLateQuartet[0]["portfolio_item_list"][0]["stats"]["asset_usd_value"]
         pool = Pool.find_by(portfolio_id: @portfolio.id, tokens: ["usd-coin", "fantom", "bitcoin", "ethereum"])
         pool.current_price = lateQuarterValue
