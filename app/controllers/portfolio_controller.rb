97382d2b7e11071bdb47102af9df4ff9c115ae7c
Adrian Chin <syncing95@gmail.com>
2022-06-05 14:47:13 +0800

    improve binance wallet total tracking

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -8,6 +8,17 @@ class PortfolioController < ApplicationController
         @inactive_pools = all_pools.select{ |pool| !pool[:is_active] }
         @farm_total = @pools.sum(&:current_price)
         @wallet = Wallet.find_by(portfolio_id: @portfolio.id, wallet_type: "Binance")
-        @portfolio_total = @wallet.total_amount + @farm_total        
+        @wallet_total = get_total_amount_in_usd(@wallet)
+        @portfolio_total = @wallet_total + @farm_total        
+    end
+
+    private 
+
+    def get_total_amount_in_usd(wallet)
+        total_amount = 0
+        @wallet.tokens.each do |token|
+            total_amount += token.amount * token.coin.price
+        end
+        total_amount
     end
 end
--- app/services/binance_wallet_updater.rb
+++ app/services/binance_wallet_updater.rb
@@ -13,6 +13,8 @@ class BinanceWalletUpdater < ApplicationService
 
     savings[0][:totalAmount]
 
+    wallet_spot_balance = client.account(recvWindow: 10_000)[:balances]
+
     snapshot = client.account_snapshot(type: 'SPOT')[:snapshotVos].last    
     
     wallet = Wallet.find(@portfolio.binance_wallet_id)
@@ -20,14 +22,15 @@ class BinanceWalletUpdater < ApplicationService
     wallet.total_amount = (snapshot[:data][:totalAssetOfBtc].to_f + BinanceSavingsUpdater.call(@portfolio)) * Coin.find_by(symbol: 'Btc').price
     wallet.save
 
-    filtered_balances = snapshot[:data][:balances].select { |token| WHITELISTED_TOKENS.include?(token[:asset]) }
+    filtered_balances = wallet_spot_balance.select { |token| WHITELISTED_TOKENS.include?(token[:asset]) }
+    puts "#{snapshot[:data].to_json}"
 
-    filtered_balances.each do |token|
+    filtered_balances.each do |token|      
       created_token = Token.find_or_initialize_by(symbol: token[:asset], wallet_id: wallet.id)
       created_token.amount = token[:free].to_f + add_btc_savings(token) 
       created_token.coin_id = Coin.find_by(symbol: token[:asset].titlecase).id
       created_token.save
-    end    
+    end
   end
 
   private
